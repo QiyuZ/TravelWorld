c3817007aa4de68c0aee3ac7f84c8061cbd42325
Qiyu <qz5uw@virginia.edu>
2018-01-08 04:05:57 +0000

    delpoy

---
+++ routes/index.js
@@ -0,0 +1,87 @@
+var express = require("express");
+var router  = express.Router();
+var passport = require("passport");
+var User = require("../models/user");
+var Campground = require("../models/campground");
+
+//root route
+router.get("/", function(req, res){
+    res.render("landing");
+});
+
+// show register form
+router.get("/register", function(req, res){
+   res.render("register", {page: 'register'}); 
+});
+
+//handle sign up logic
+router.post("/register", function(req, res){
+    var newUser = new User({
+        username: req.body.username,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        avatar: req.body.avatar
+      });
+
+    if(req.body.adminCode === 'secretcode123') {
+      newUser.isAdmin = true;
+    }
+
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register", {error: err.message});
+        }
+        passport.authenticate("local")(req, res, function(){
+           req.flash("success", "Successfully Signed Up! Nice to meet you " + req.body.username);
+           res.redirect("/campgrounds"); 
+        });
+    });
+});
+
+//show login form
+router.get("/login", function(req, res){
+   res.render("login", {page: 'login'}); 
+});
+
+//handling login logic
+router.post('/login', function(req, res, next) {
+  passport.authenticate('local', function(err, user, info) {
+    if (err) { return next(err); }
+    if (!user) { return res.redirect('/login'); }
+    req.logIn(user, function(err) {
+      if (err) { return next(err); }
+      var redirectTo = req.session.redirectTo ? req.session.redirectTo : '/campgrounds';
+      delete req.session.redirectTo;
+      res.redirect(redirectTo);
+    });
+  })(req, res, next);
+});
+
+// logout route
+router.get("/logout", function(req, res){
+   req.logout();
+   req.flash("success", "See you later!");
+   res.redirect("/campgrounds");
+});
+
+// USER PROFILE
+router.get("/users/:id", function(req, res) {
+  User.findById(req.params.id, function(err, foundUser) {
+    if(err) {
+      req.flash("error", "Something went wrong.");
+      res.redirect("/");
+    }
+    Campground.find().where('author.id').equals(foundUser._id).exec(function(err, campgrounds) {
+      if(err) {
+        req.flash("error", "Something went wrong.");
+        res.redirect("/");
+      }
+      res.render("users/show", {user: foundUser, campgrounds: campgrounds});
+    })
+  });
+});
+
+
+module.exports = router;
